5b4578cc7805acc1a16d9901ee87d87b2d8b9689
parksangseok <park0157@naver.com>
2026-05-04 16:59:43 +0900

    commit

--- js/common.js
+++ js/common.js
@@ -1,4 +1,20 @@
-$(function(){
+$(function () {
+
+  //드롭다운
+  $('.toggleBtn').click(function (e) {
+    e.stopPropagation();
+    $(this).parent().toggleClass('active');
+
+    if ($(this).parent().hasClass('active')) {
+      $(this).attr('aria-expanded', 'true');
+    } else {
+      $(this).attr('aria-expanded', 'false');
+    }
+  });
+  $('.selectCloseBtn').click(function () {
+    $(this).parents('.toggleBtnBox').removeClass('active');
+    $(this).parents('.toggleBtnBox').find('.toggleBtn').attr('aria-expanded', 'false').focus();
+  });
 
 
 });
